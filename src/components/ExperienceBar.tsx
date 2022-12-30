8d0c75f80baeed6f06f8b7591e821099d7edcace
Wagner Carestini <wcarestini@gmail.com>
2022-12-30 14:10:07 -0300

    config next.

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,16 @@
+import style from "../styles/ExperienceBar.module.css";
+
+export function ExperienceBar() {
+  return (
+    <header className={style.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }} />
+        <span className={style.currentExperience} style={{ left: "50%" }}>
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
